7b7863c1f4efae50d33dfa100d6430084b032919
Lulu <ldal48848@edu.ucl.dk>
2025-03-11 09:55:19 +0100

    Init project

---
+++ firebase-test/src/configs/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdtjUx2_3bYHok3HZAIHHspwVvjsIUsi4",
+  authDomain: "mfd2025-c38c7.firebaseapp.com",
+  projectId: "mfd2025-c38c7",
+  storageBucket: "mfd2025-c38c7.firebasestorage.app",
+  messagingSenderId: "63536003708",
+  appId: "1:63536003708:web:6cc5a05bb9759b503527f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
